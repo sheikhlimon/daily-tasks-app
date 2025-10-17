605ba0bd84092db6202d87886572a7f186929756
sheikhlimon <sheikhlimon404@gmail.com>
2025-10-17 16:11:43 +0600

    Added data structure for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+type Priority = "p1" | "p2" | "p3";
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Hello World!</div>;
+  const tasks: Task[] = [
+    { id: 1, title: "Learn React", isCompleted: false },
+    { id: 2, title: "Learn TypeScript", isCompleted: false },
+  ];
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
