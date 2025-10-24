2d4fe69e39aee1beeda1ccef4c5b9aea510ae935
sheikhlimon <sheikhlimon404@gmail.com>
2025-10-24 18:38:44 +0600

    Setup test for rendering App

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App.tsx";
+
+describe("App", () => {
+  test("should render input fields and app button", () => {
+    render(<App />);
+
+    const input = screen.getByRole("textbox", { name: "Add Task:" });
+    const button = screen.getByRole("button", { name: "Add" });
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
