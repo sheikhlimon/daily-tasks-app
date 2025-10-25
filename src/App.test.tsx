a2a7a8f6a8f76e2cb74d903cf349c6b4a2159320
sheikhlimon <sheikhlimon404@gmail.com>
2025-10-25 14:36:01 +0600

    Added test for clearing input after adding a task

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App.tsx";
 
 describe("App", () => {
@@ -11,19 +12,35 @@ describe("App", () => {
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
   });
-  test("should add task to list when add button is clicked", () => {
+
+  test("should add task to list when add button is clicked", async () => {
     render(<App />);
+
     const input = screen.getByRole("textbox", { name: "Add Task:" });
     const button = screen.getByRole("button", { name: "Add" });
 
-    fireEvent.change(input, {
-      target: {
-        value: "New Task",
-      },
-    });
+    await userEvent.type(input, "New Task");
 
-    fireEvent.click(button);
+    await userEvent.click(button);
 
     expect(screen.getByText("New Task")).toBeInTheDocument();
   });
+
+  test("should clear input field after adding a task", async () => {
+    render(<App />);
+
+    const input = screen.getByRole("textbox", { name: "Add Task" });
+
+    const button = screen.getByRole("button", { name: "Add " });
+
+    await userEvent.type(input, "New Task");
+
+    await userEvent.click(button);
+
+    expect(input).toHaveValue("");
+  });
+
+  test.todo("should not add an empty task", () => {});
+
+  test.todo("should add a task by enter key", () => {});
 });
